c3397a575e3194216cc600b1d4df30ac58c600ed
ayush gupta <ayushkr790@gmail.com>
2025-12-30 22:32:09 +0530

    api structuring

--- apps/web/app/services/auth.service.ts
+++ apps/web/app/services/auth.service.ts
@@ -1,15 +1,17 @@
 import { LoginFormValues, User, UserType } from "@repo/common/types";
 import { loginApi, signupApi } from "app/api/auth.api";
+import { error } from "console";
+import { promises } from "dns";
 
 export const loginService = async (
   loginData: LoginFormValues
 ): Promise<UserType> => {
   const res = await loginApi(loginData);
-  if (!res.data) throw new Error("Empty response form server");
+  if (!res.data) throw { message: "Empty response from server" };
   const parsedData = User.safeParse(res.data);
   if (!parsedData.success) {
     console.error(parsedData.error);
-    throw new Error("Invalid login response from server");
+    throw { message: "Invalid login response from server" };
   }
   return res.data;
 };
